338b3e31eb72199974472aa768659cbfce3acfaa
YULitle <71101155+YULitle@users.noreply.github.com>
2020-09-24 10:17:41 -0500

    Create project1.cpp

---
+++ project1.cpp
@@ -0,0 +1,22 @@
+int main() {
+    int factorial;
+    cin >> factorial;
+    int product = 1;
+    //changed greater than to less than
+    if (factorial < 10){
+        //changed i to = 1
+        //changed i to be <= to factorial from > factorial
+        //changed to ++ from --
+        for (int i = 1; i <= factorial; i++) {
+            //you already have something adding to i so just multiply
+            product = product * i;
+        }
+        
+    } else {
+        cout << "Your input is too large. Try again.";
+        return 0;
+    }
+    //output the prduct not input
+    cout << product;
+    return factorial;
+}
